f96043c7e34208e8f9bcd1f00665ca0aa2962b50
arthurmenezesb <arthur.menezes89@gmail.com>
2025-11-28 17:57:39 -0300

    chore: initialize project structure and configuration

---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "antd";
+
+const { Title } = Typography;
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Title>SWStarter</Title>
+    </div>
+  );
+};
+
+export default Home;
